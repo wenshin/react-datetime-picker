1aa0d31b104d1cbb75e120d7ae272c5483cb381b
yuanwen <wenshin2011@gmail.com>
2016-11-16 21:31:03 +0800

    period.release: version 1.0.1 and examples

--- docs/index.entry.js
+++ docs/index.entry.js
@@ -4,23 +4,48 @@ import moment from 'moment';
 
 import './init';
 import '../lib/style.css';
-import DatetimePicker from '../lib';
+import DatetimePicker, {Period} from '../lib';
 
 
 class App extends Component {
   state = {
-    datetime: moment()
+    datetime: moment(),
+    period: {}
   }
 
   render() {
+    console.log(this.state.period)
     return (
-      <DatetimePicker
-        style={{width: '300px'}}
-        value={this.state.datetime}
-        format='YYYY-MM-DD HH:mm:ss'
-        limit={{min: '2016-10-10 12:00:00', max: '2017-01-01 20:30:30'}}
-        onChange={datetime => this.setState({datetime})}
-      />
+      <div>
+        <article>
+          <section>
+            <h1>DateTimePicker</h1>
+            <div>
+              <DatetimePicker
+                style={{width: '300px'}}
+                value={this.state.datetime}
+                format='YYYY-MM-DD HH:mm:ss'
+                limit={{min: '2016-10-10 12:00:00', max: '2017-01-01 20:30:30'}}
+                onChange={datetime => this.setState({datetime})}
+              />
+            </div>
+          </section>
+        </article>
+        <article>
+          <section>
+            <h1>Period</h1>
+            <div>
+              <Period
+                style={{width: '300px'}}
+                value={this.state.period}
+                format='YYYY-MM-DD HH:mm:ss'
+                limit={{min: '2016-10-10 12:00:00', max: '2017-01-01 20:30:30'}}
+                onChange={period => this.setState({period})}
+              />
+            </div>
+          </section>
+        </article>
+      </div>
     );
   }
 }
