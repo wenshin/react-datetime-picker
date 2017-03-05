924c96fc080851aa805d7a8558ff742493c9154d
yuanwen <wenshin2011@gmail.com>
2017-03-05 22:54:52 +0800

    Docs: change format of period

--- docs/index.entry.js
+++ docs/index.entry.js
@@ -3,17 +3,29 @@ import ReactDOM from 'react-dom';
 import moment from 'moment';
 
 import './init';
-import DatetimePicker, {Period} from '../lib';
+import DatetimePicker, {Period, Util} from '../lib';
 import '../lib/style.css';
 
 
 class App extends Component {
   state = {
     datetime: moment(),
-    period: {}
+    period: {},
+    periodFormat: 'YYYY-MM-DD HH:mm:ss'
+  };
+
+  changePeriodFormat = (e) => {
+    const {period} = this.state;
+    const format = e.target.value;
+    const typeObj = Util.parseFormat(format);
+    period.start && period.start.startOf(typeObj.unit);
+    period.end && period.end.endOf(typeObj.unit);
+    this.setState({periodFormat: format, period});
   }
 
   render() {
+    const {period, periodFormat} = this.state;
+    console.log(period.start, period.end);
     const limit = {
       min: moment().subtract(5, 'day'),
       max: moment().add(5, 'day')
@@ -37,13 +49,24 @@ class App extends Component {
         <article>
           <section>
             <h1>Period</h1>
+            <select onChange={this.changePeriodFormat}>
+              <option value='YYYY-MM-DD HH:mm:ss'>Datetime</option>
+              <option value='YYYY-MM-DD'>Date</option>
+              <option value='YYYY-MM'>Month</option>
+              <option value='YYYY'>Year</option>
+              <option value='HH:mm:ss'>Time</option>
+              <option value='HH'>Hour</option>
+              <option value='HH:mm'>Minute</option>
+              <option value='HH:mm:ss'>Second</option>
+            </select>
             <div>
               <Period
+                startEndOf
                 style={{width: '300px'}}
-                value={this.state.period}
-                format='YYYY-MM-DD HH:mm:ss'
+                value={period}
+                format={periodFormat}
                 limit={limit}
-                onChange={period => this.setState({period})}
+                onChange={p => this.setState({period: p})}
               />
             </div>
           </section>
